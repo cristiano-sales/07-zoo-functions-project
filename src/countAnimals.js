b5ba0c6c84743214e5d8fa28a26e89b1393da1d1
Cristiano Sales <cristianosalesneves@gmail.com>
2022-01-07 18:44:04 -0300

    solve countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
-const data = require('../data/zoo_data');
+//  const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  if (!animal.sex) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  return species.find((specie) => specie.name === animal.specie).residents
+    .reduce((acc, cur) => {
+      if (animal.sex === cur.sex) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
