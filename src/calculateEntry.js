6c380f9072801bc92d110e9e500ed753c06f1445
Cristiano Sales <cristianosalesneves@gmail.com>
2022-01-10 15:16:16 -0300

    solve countEntrants and calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acc, cur) => {
+    if (cur.age < 18) acc.child += 1;
+    if (cur.age >= 18 && cur.age < 50) acc.adult += 1;
+    if (cur.age >= 50) acc.senior += 1;
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !Object.keys(entrants).length) return 0;
+  const { child, adult, senior } = countEntrants(entrants);
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
