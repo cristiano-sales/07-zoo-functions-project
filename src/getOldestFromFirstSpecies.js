3ffdc89fcd8548b46769e218f640540789f8428d
Cristiano Sales <cristianosalesneves@gmail.com>
2022-01-10 20:36:43 -0300

    solve getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
-const data = require('../data/zoo_data');
+//  Ref.: Rafael França
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees, species } = require('../data/zoo_data');
+
+const getOldestFromFirstSpecies = (id) => {
+  const arrayResponsibleFor = employees.find((employee) => employee.id === id).responsibleFor;
+  const residentsOfFirstSpecie = species.find(
+    (specie) => specie.id === arrayResponsibleFor[0],
+  ).residents;
+  return Object.values( //  para retornar uma array com os valores das chaves e não todo o objeto
+    residentsOfFirstSpecie.reduce((previous, next) => (previous.age > next.age ? previous : next)),
+  );
+};
 
 module.exports = getOldestFromFirstSpecies;
